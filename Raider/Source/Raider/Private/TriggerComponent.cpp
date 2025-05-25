a6d9366d12b4ba4068ce826b5a26ea9e14799c93
Akay729 <alecmularoni@gmail.com>
2025-05-25 21:14:02 +0200

    Raider: Creating TriggerComponet

--- Raider/Source/Raider/Private/TriggerComponent.cpp
+++ Raider/Source/Raider/Private/TriggerComponent.cpp
@@ -3,3 +3,25 @@
 
 #include "TriggerComponent.h"
 
+
+//Questo costruttore serve principalmente principalmente per attivare tick 
+UTriggerComponent::UTriggerComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+    UE_LOG(LogTemp, Display, TEXT("TriggerComponent"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    UE_LOG(LogTemp, Display, TEXT("TriggerComponent ticking"));
+}
--- Raider/Source/Raider/Public/TriggerComponent.h
+++ Raider/Source/Raider/Public/TriggerComponent.h
@@ -9,8 +9,21 @@
 /**
  * 
  */
-UCLASS()
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class RAIDER_API UTriggerComponent : public UBoxComponent
 {
 	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTriggerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 };
