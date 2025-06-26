58782c2105f676c8a2b63bb7202baa44e75f75f2
Akay729 <alecmularoni@gmail.com>
2025-06-26 22:45:26 +0200

    SimpleShooter: Implementing Creating a Costum BTNodeBase

---
+++ SimpleShooter/Source/SimpleShooter/BTTask_BlackboardValue.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_BlackboardValue.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_BlackboardValue::UBTTask_BlackboardValue()
+{
+    NodeName = TEXT("Clear Blackboard Value");
+}
+
+
+EBTNodeResult::Type UBTTask_BlackboardValue::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    Super::ExecuteTask(OwnerComp, NodeMemory);
+    FName SelectedKey = GetSelectedBlackboardKey();
+    OwnerComp.GetBlackboardComponent()->ClearValue(SelectedKey);
+    return EBTNodeResult::Succeeded;
+}
